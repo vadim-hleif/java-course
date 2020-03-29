12bf7851b745a4ff5f1a1e79f26907269eee27c2
vadim-hleif <vadzim.hleif@gmail.com>
2020-03-29 21:32:14 +0500

    feat(*): add hbs
    talk about starters

--- src/main/java/ru/vadim_hleif/course/GreetingController.java
+++ src/main/java/ru/vadim_hleif/course/GreetingController.java
@@ -1,17 +1,25 @@
 package ru.vadim_hleif.course;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.vadim_hleif.course.student.Student;
+import ru.vadim_hleif.course.student.StudentGroupProvider;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class GreetingController {
 
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+
+        return "test";
     }
 
 }
